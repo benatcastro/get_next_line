01934cc2a9d3a7dac825127dd14da3070e2d50bd
Benat Castro <becastro@c3r4s5.42urduliz.com>
2022-05-23 22:05:35 +0200

    null problem

--- get_next_line.c
+++ get_next_line.c
@@ -22,20 +22,18 @@ static	t_fd	*ft_get_node(int fd, t_fd *node)
 	return (node);
 }
 
-static	int	get_str(int fd, t_fd *node)
+static	int	get_str(int fd, t_fd *node, int rd)
 {
-	int		rd;
 	char	*buffer;
 	char	*aux;
 
-	rd = 1;
 	buffer = malloc(sizeof(char *));
 	node->str = malloc(1);
 	node->str[0] = 0;
-	while (rd != 0)
+	while (rd > 0)
 	{
 		rd = read(fd, buffer, 1);
-		if (rd == 0)
+		if (rd <= 0)
 		{
 			free(buffer);
 			return (1);
@@ -44,7 +42,7 @@ static	int	get_str(int fd, t_fd *node)
 		aux = ft_strjoin(node->str, buffer);
 		node->str = ft_strdup(aux);
 		free(aux);
-		if (node->str[ft_strlen(node->str) - 1] == '\n')
+		if (node->str[ft_strlen(node->str) - 1] == '\n' || buffer[0] == '\0')
 			break ;
 	}
 	free(buffer);
@@ -56,10 +54,10 @@ char	*get_next_line(int fd)
 	static t_fd	*node;
 	int			eof;
 
-	if (fd < 0 || BUFFER_SIZE < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 )
 		return (NULL);
 	node = ft_get_node(fd, node);
-	eof = get_str(fd, node);
+	eof = get_str(fd, node, 1);
 	if (eof == 1)
 	{
 		free(node->str);
@@ -77,7 +75,8 @@ int	main(void)
 	int		fd;
 	char	*str;
 
-	fd = open("tests/alternate_line_nl_with_nl", O_RDONLY);
+	fd = open("tests/41_no_nl", O_RDONLY);
+	printf("FD: (%d)\n", fd);
 	for (size_t i = 0; i < 15; i++)
 	{
 		str = get_next_line(fd);
